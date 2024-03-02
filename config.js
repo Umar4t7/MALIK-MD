419f798604a4430b7b498084f22a4bd62c490c53
Umar4t7 <158988215+Umar4t7@users.noreply.github.com>
2024-03-02 14:32:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,19 +9,19 @@ global.port= process.env.PORT || 5000
 global.email = 'akk728492@gmail.com'
 global.github = 'https://github.com/arkhan998/MALIK-MD'
 global.location = 'Pakistan'
-global.gurl = 'king-md.db' // add your username
+global.gurl = 'umar-md.db' // add your username
 global.sudo = process.env.SUDO || '923124533358'
-global.devs = '923124533358';
+global.devs = '233200857544';
 global.website = 'https://github.com/arkhan998/MALIK-MD' //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/dce77f7538019d05afe65.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/31ae33bf9bf715a84c359.jpg'
 module.exports = {
   
   sessionName:  process.env.SESSION_ID ||  "session",
-  botname:   process.env.BOT_NAME === undefined ? 'MALIK-BOT' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'arkhan' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '*«ᴍᴀᴅᴇ ʙʏ ᴜᴍᴀʀ»*' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'HANIA' : process.env.OWNER_NAME,
   author:  process.env.PACK_INFO === undefined ? 'bot' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO === undefined ? 'malik-Md' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO === undefined ? 'umar' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nigga' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
@@ -33,11 +33,23 @@ recordingpresence:  process.env.RECORDING === undefined ? false : process.env.RE
   HANDLERS:  process.env.PREFIX === undefined ? '.' : process.env.PREFIX,
   warncount : process.env.WARN_COUNT === undefined ? 3 : process.env.WARN_COUNT,
   disablepm:  process.env.DISABLE_PM === undefined ? false : process.env.DISABLE_PM,
-  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? true : process.env.LEVEL_UP_MESSAGE,
+  levelupmessage:  process.env.LEVEL_UP_MESSAGE === undefined ? false : process.env.LEVEL_UP_MESSAGE,
   antilink:  process.env.ANTILINK_VALUES === undefined ? 'chat.whatsapp.com' : process.env.ANTILINK_VALUES,
   antilinkaction: process.env.ANTILINK_ACTION === undefined ? 'remove' : process.env.ANTILINK_ACTION,
-  BRANCH: 'main', 
-  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '' : process.env.ALIVE_MESSAGE,
+  BRANCH: 'main by umar', 
+  ALIVE_MESSAGE:  process.env.ALIVE_MESSAGE === undefined ? '┌───────────────
+│ 👨🏻‍💻 Owner:- *UMAR*
+│ 🤙🏻 *CONTACT:-* *+923121870553*
+│ 📱 *Device:-* *ANDROID* 
+└───────────────
+
+*Hey Bot is Online Now👨🏻‍💻* 
+
+┌───────────────
+ | 🔖 *Bot Online:-* *(.ALIVE)*
+ | 🔖 *Command:-* *(.MENU)*
+ | 🔖 *Bot Speed:-* *(.ping)*
+└───────────────' : process.env.ALIVE_MESSAGE,
   afk:  process.env.AFK ||true,
   autobio:  process.env.AUTO_BIO === undefined ? '' : process.env.AUTO_BIO,
   OPENAI_API_KEY:  process.env.OPENAI_API_KEY === undefined ? false : process.env.OPENAI_API_KEY,
@@ -47,7 +59,7 @@ recordingpresence:  process.env.RECORDING === undefined ? false : process.env.RE
     API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
     APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
 },
-  VERSION: process.env.VERSION === undefined ? 'v.0.0.3' : process.env.VERSION,
+  VERSION: process.env.VERSION === undefined ? 'v1.0' : process.env.VERSION,
   LANG: process.env.THEME|| 'MALIK',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'private' : process.env.WORKTYPE
 };
